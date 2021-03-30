a046a9036aad76618f74512a060a0df27e4bf0e5
Brian J. Ball <brianbll9@gmail.com>
2021-03-30 09:35:56 -0400

    fix lyrics, add spectrum, play first record, add music assets

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,38 +1,122 @@
 import React, { Component } from 'react';
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Container } from 'react-bootstrap';
 
 import './MainContainer.css'
 
 import foreverYaGirl from '../assets/forever-ya-girl.png';
 import mainImg from '../assets/main-img.png';
+import keiyaaLogo from '../assets/keiyaa-logo.png';
+
+import previousIcon from '../assets/icons/skip_previous_black_24dp.svg'; 
+import playButton from '../assets/icons/play_arrow_black_24dp.svg';
+import nextIcon from '../assets/icons/skip_next_black_24dp.svg';
 
 class Player extends Component {
     render() {
         return (
-            <div className="audioPlayer">
-                {/* previous icon */}
-                {/* play icon */}
-                {/* next icon */}
-            </div>
+            <>
+                <Col sm={3}>
+                    <img src={previousIcon} className="audioPlayer--skip" id="previous--icon" alt="previous-track"/>
+                </Col>   
+                <Col sm={6}>
+                    <img src={playButton} className="audioPlayer--play" id="play--icon" alt="play-track"/>
+                </Col> 
+                <Col sm={3}>
+                    <img src={nextIcon} className="audioPlayer--skip" id="next--icon" alt="next-track" />
+                </Col> 
+            </>
+        )
+    }
+}
+
+class Lyrics extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            displayLyrics: this.props.display,
+        }
+    }
+
+    render() {
+        return (
+            <>
+                <div className="lyrics-cover">
+                    <div className="lyrics-text noshow" id="lyrics-text">
+                    why won't you love me?
+                    i'm so damn easy to love
+                    i'm so damn easy to love
+                    i'm so damn easy to trust
+                    i please you enough
+                    i know that reason is tough to buck with...
+
+                    talk to me.
+                    </div>
+                </div>
+           </>
         )
     }
 }
 
 export default class MainContainer extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            displayLyrics: false,
+        }
+    }
+
+    // setLyricsDisplayState() {
+    //     if (!this.state.displayLyrics) {
+    //         this.setState({ displayLyrics: true });
+    //     } else {
+    //         this.setState({ displayLyrics: false });
+    //     }
+    // }
 
     render() {
         return(
             <>
-            <div className="mainContainer">
-            <Row fluid>
-                <Col sm={8}>
-                    <img src={mainImg} alt="forever-ya-girl-cover-art"/>
-                </Col>
-                <Col sm={4}>
-                    <img src={foreverYaGirl} alt="forever-ya-girl"/>
-                </Col>
-                
-            </Row>
+            <div className="mainContainer" >
+            <Container fluid>
+                <Row style={{height: '80vh', overflow: 'hidden'}}>
+                    <Col style={{height: '100%', overflow: 'hidden'}} sm={8}>
+                        <Lyrics display={this.state.displayLyrics}/>
+                        <img src={mainImg} alt="forever-ya-girl-cover-art"/>
+                    </Col>
+                    <Col sm={4} style={{ padding: '30px' }}>
+                       <Row style={{ marginBottom: '50px' }}>
+                           <img style={{ display: 'block', margin: 'auto' }} src={foreverYaGirl} alt="forever-ya-girl"/>
+                           </Row>
+                       <Row style={{textAlign: 'center'}}> 
+                            <Player />
+                       </Row>
+                       <Row>
+                            <div className="modal-player" style={{width: '100%'}}>
+                            <div style={{margin: 'auto'}} type="button" id="btn-play"
+                                    ></div>
+                                <div style={{margin: 'auto', width: '100%'}}> 
+                                    <div className="audio-spectrum" id="audio-spectrum"></div>
+                                </div>
+                            </div>
+                        </Row>
+                       <Row style={{ width: '100%', position: 'absolute', bottom: '100px' }}> 
+                        <span className="lyrics-toggle-button lato-font" id="lyrics-toggle">LYRICS</span> 
+                       </Row>
+                    </Col>
+                </Row>
+                <Row style={{height: '20vh', width: '100%'}}>
+                    <Col style={{color: 'white', height: '100%'}} sm={8}>
+                        <h3 style={{ marginBottom: 0 }} className={'lato-font'}>NOW PLAYING</h3>
+                        <div className={'track-title-showbox'}>
+                            <span className="track-title">I Thot There Was One Wound in This House, There's Two</span>
+                        </div>
+                    </Col>
+                    <Col style={{height: '100%'}} sm={4}>
+                        <img style={{position: 'absolute', left: '-200px', bottom: '0'}} src={keiyaaLogo} alt="keiyaa-name-logo"/>
+                    </Col>
+                </Row>
+            </Container>
             </div>
             
             </>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import WaveSurfer from 'wavesurfer.js';
+
+import firstTrack from '../src/assets/keiyaa--forever-ya-girl/01-i-thot-there-was-one-wound-in-this-house-theres-two.mp3'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,6 +13,69 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+      
+var Spectrum = WaveSurfer.create({
+  container: '#audio-spectrum',
+  progressColor: "#ffffff",
+  height: 50,
+autoCenter: true
+});
+
+var buttons = {
+  play: document.getElementById("play--icon"),
+//                                     pause: document.getElementById("btn-pause"),
+//                                     stop: document.getElementById("btn-stop")
+};
+
+const lyricsText = document.getElementById('lyrics-text'); 
+const lyricsToggle = document.getElementById('lyrics-toggle');
+
+lyricsToggle.addEventListener("click", function () {
+  if(lyricsText.classList.contains('noshow')) {
+    lyricsText.classList.remove('noshow')
+  } else {
+    lyricsText.classList.add('noshow')
+  }
+});
+
+//Handle Play button
+buttons.play.addEventListener("click", function () {
+  if(Spectrum.pause){
+              Spectrum.play();
+              buttons.innerHTML = "||";
+            } else {
+              Spectrum.pause();
+              buttons.innerHTML = ">";
+            }
+});
+
+//                                 // Add a listener to enable the play button once it's ready
+Spectrum.on('ready', function () {
+  buttons.play.disabled = false;
+});
+
+//                                 // If you want a responsive mode (so when the user resizes the window)
+//                                 // the spectrum will be still playable
+// window.addEventListener("resize", function () {
+//   // Get the current progress according to the cursor position
+//   var currentProgress = Spectrum.getCurrentTime() / Spectrum.getDuration();
+
+//   // Reset graph
+//   Spectrum.empty();
+//   Spectrum.drawBuffer();
+//   // Set original position
+//   Spectrum.seekTo(currentProgress);
+
+//   // Enable/Disable respectively buttons
+//   buttons.pause.disabled = true;
+//   buttons.play.disabled = false;
+//   buttons.stop.disabled = false;
+// });
+
+// Load the audio file from your domain !
+Spectrum.load(firstTrack);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
